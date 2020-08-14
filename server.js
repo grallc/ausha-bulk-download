641eaaece3d7d673e8b3cec7b077372d30424c4d
Corentin Grall <corentin.grall@gmail.com>
2020-08-14 23:56:41 +0200

    Started download func

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+
+const downloadFiles = async (podcastLink) => {
+  console.log('Starting downloading files...')
+  const html = await axios.get(podcastLink)
+  const $ = cheerio.load(html.data)
+  console.log('Successfully loaded HTML...')
+}
+downloadFiles()
