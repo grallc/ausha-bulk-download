0a4584a7567dd8aee31842be9ac62650da78ff80
Corentin Grall <corentin.grall@gmail.com>
2020-08-15 00:14:40 +0200

    Argument handling

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const colors = require('colors')
 
-const downloadFiles = async (podcastLink) => {
-  console.log('Starting downloading files...')
+const podcastLink = process.argv[2]
+
+const downloadPodcast = async () => {
+  console.log(colors.green(`Starting downloading ${colors.bold(podcastLink)}${colors.green('...')}`))
   const html = await axios.get(podcastLink)
   const $ = cheerio.load(html.data)
   console.log('Successfully loaded HTML...')
 }
-downloadFiles()
+
+if (!podcastLink || !podcastLink.includes('https://podcast.ausha.co/')) {
+  console.log(colors.red(colors.bold('Invalid URL!')))
+} else {
+  downloadPodcast()
+}
